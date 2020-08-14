4aececfd95419c8802c8576045a615139ad08b0e
Grupo 3 <naelsito@gmail.com>
2020-08-14 20:36:31 -0300

    Listados de productos (add)

--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
+var categoriesArray = [];
+showSpinner()
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let product = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + product.imgSrc + `" alt="` + product.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ product.name + ' - ' + product.cost + ' ' + product.currency + `</h4>
+                        <small class="text-muted">` + product.soldCount + ` artículos</small>
+                        
+
+                    </div>
+                <small class="text-muted">` + product.description + `</small>
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+        hideSpinner()
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }
+    });
 });
